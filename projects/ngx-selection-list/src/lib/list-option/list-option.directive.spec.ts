6e167feb913488ba6f2f784bd4191199ab238007
Alexander Merz <alex96merz@gmx.de>
2023-10-01 22:47:08 +0200

    test: list option should change selected state upon click

--- projects/ngx-selection-list/src/lib/list-option/list-option.directive.spec.ts
+++ projects/ngx-selection-list/src/lib/list-option/list-option.directive.spec.ts
@@ -1,7 +1,22 @@
+import { fireEvent, render, screen } from '@testing-library/angular';
+
 import { ListOptionDirective } from "./list-option.directive";
 
 describe(ListOptionDirective.name, () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
+  it('should change selected state upon click', async () => {
+    await render('<span ngxListOption [value]="1">Option 1</span>', {
+      imports: [ListOptionDirective],
+    });
+
+    const option = screen.getByRole('option');
+
+    expect(option).toHaveAccessibleName('Option 1');
+    expect(option).not.toHaveAttribute('selected');
+    expect(option).toHaveAttribute('aria-selected', 'false');
+
+    fireEvent.click(option);
+
+    expect(option).toHaveAttribute('selected', 'selected');
+    expect(option).toHaveAttribute('aria-selected', 'true');
   })
 });
--- projects/ngx-selection-list/src/lib/selection-list/selection-list.directive.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SelectionListDirective } from "./selection-list.directive";
-
-describe(SelectionListDirective.name, () => {
-   it('should pass', () => {
-    expect(true).toBe(true);
-  })
-});
