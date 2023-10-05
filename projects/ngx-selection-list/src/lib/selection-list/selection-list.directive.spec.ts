537e3c4e284bb84373368d67dec349ab6ae0a7fc
Alexander Merz <alex96merz@gmx.de>
2023-10-05 07:51:39 +0200

    test: initial and empty value of null

--- projects/ngx-selection-list/src/lib/selection-list/selection-list.directive.spec.ts
+++ projects/ngx-selection-list/src/lib/selection-list/selection-list.directive.spec.ts
@@ -1,3 +1,5 @@
+import { Component, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { fireEvent, render, screen } from '@testing-library/angular';
 import { ListOptionDirective } from '../list-option/list-option.directive';
 import { SelectionListDirective } from './selection-list.directive';
@@ -69,5 +71,73 @@ describe(SelectionListDirective.name, () => {
     fireEvent.click(lastOption);
 
     expect(selectionList).toHaveAttribute('value', '1,3');
+  });
+
+  it('should have an initial and empty value of null for single selection', async () => {
+    @Component({
+      standalone: true,
+      imports: [ListOptionDirective, SelectionListDirective],
+      template: `
+        <div ngxSelectionList>
+          <span ngxListOption [value]="1">Option 1</span>
+        </div>
+      `,
+    })
+    class TestComponent {
+      @ViewChild(SelectionListDirective) readonly selectionList!: SelectionListDirective;
+      @ViewChildren(ListOptionDirective) readonly listOptions!: QueryList<ListOptionDirective>;
+    }
+
+    await TestBed.configureTestingModule({ imports: [TestComponent] }).compileComponents();
+
+    const fixture = TestBed.createComponent(TestComponent);
+    const component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    expect(component.selectionList.value).toBeNull();
+
+    component.listOptions.first.select();
+
+    expect(component.selectionList.value).toBe(1);
+
+    component.listOptions.first.deselect();
+
+    expect(component.selectionList.value).toBeNull();
+  });
+
+  it('should have an initial and empty value of null for multi selection', async () => {
+    @Component({
+      standalone: true,
+      imports: [ListOptionDirective, SelectionListDirective],
+      template: `
+        <div ngxSelectionList [multiple]="true">
+          <span ngxListOption [value]="1">Option 1</span>
+          <span ngxListOption [value]="2">Option 2</span>
+          <span ngxListOption [value]="3">Option 3</span>
+        </div>
+      `,
+    })
+    class TestComponent {
+      @ViewChild(SelectionListDirective) readonly selectionList!: SelectionListDirective;
+      @ViewChildren(ListOptionDirective) readonly listOptions!: QueryList<ListOptionDirective>;
+    }
+
+    await TestBed.configureTestingModule({ imports: [TestComponent] }).compileComponents();
+
+    const fixture = TestBed.createComponent(TestComponent);
+    const component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    expect(component.selectionList.value).toBeNull();
+
+    component.listOptions.first.select();
+    component.listOptions.last.select();
+
+    expect(component.selectionList.value).toStrictEqual([1, 3]);
+
+    component.listOptions.first.deselect();
+    component.listOptions.last.deselect();
+
+    expect(component.selectionList.value).toBeNull();
   });
 });
